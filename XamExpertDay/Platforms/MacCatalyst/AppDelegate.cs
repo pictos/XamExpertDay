4345e52357c375589d77ad818eed9893db97bf33
Pedro Jesus <pedrojesus.cefet@gmail.com>
2021-09-30 18:53:19 -0300

    Add project files.

---
+++ XamExpertDay/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace XamExpertDay
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
